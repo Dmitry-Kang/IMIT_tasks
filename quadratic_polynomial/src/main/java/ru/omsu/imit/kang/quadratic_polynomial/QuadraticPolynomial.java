c25c31ff725376c96fa06013d10f56a121bc732f
Dmitriy Alexandrov <alexandrov.dp@mail.ru>
2018-11-11 18:55:25 +0600

    Add quadratic poly project

---
+++ quadratic_polynomial/src/main/java/ru/omsu/imit/kang/quadratic_polynomial/QuadraticPolynomial.java
@@ -0,0 +1,18 @@
+package ru.omsu.imit.kang.quadratic_polynomial;
+
+public class QuadraticPolynomial {
+
+    private double a,b,c;
+
+    public QuadraticPolynomial(double a, double b, double c){
+        if(a == 0){
+            throw new IllegalArgumentException("a must be not zero");
+        }
+        this.a = a;this.b = b;this.c = c;
+    }
+    public double[] solve(){
+        //TODO
+        return null;
+    }
+    
+}
