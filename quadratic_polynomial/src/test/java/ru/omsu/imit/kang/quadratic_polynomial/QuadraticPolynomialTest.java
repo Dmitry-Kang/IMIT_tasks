34875126b712e814c1a9b3295c3d627b85dd9426
Dmitriy Alexandrov <alexandrov.dp@mail.ru>
2018-12-02 13:28:39 +0600

    Set up new project

---
+++ quadratic_polynomial/src/test/java/ru/omsu/imit/kang/quadratic_polynomial/QuadraticPolynomialTest.java
@@ -0,0 +1,12 @@
+package ru.omsu.imit.kang.quadratic_polynomial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuadraticPolynomialTest {
+
+    @Test
+    public void solve() {
+    }
+}
