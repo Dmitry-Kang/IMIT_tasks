bf1221501c0cdcc1d2c6255895f29078d6c824f7
Dmitriy Alexandrov <alexandrov.dp@mail.ru>
2018-12-02 15:06:47 +0600

    Added tests for Polynomial Processor

--- other_tasks/src/main/java/ru/omsu/imit/kang/other_tasks/PolynomialProcessor.java
+++ other_tasks/src/main/java/ru/omsu/imit/kang/other_tasks/PolynomialProcessor.java
@@ -5,6 +5,9 @@ import ru.omsu.imit.kang.quadratic_polynomial.QuadraticPolynomial;
 public class PolynomialProcessor {
     private QuadraticPolynomial a;
 
+    public PolynomialProcessor(QuadraticPolynomial a) {
+        this.a = a;
+    }
     public double getMaxRoot() throws PolynomialProcessorException{
         double[] roots = a.solve();
         if(roots.length == 0){
--- other_tasks/src/test/java/ru/omsu/imit/kang/other_tasks/PolynomialProcessorTest.java
+++ other_tasks/src/test/java/ru/omsu/imit/kang/other_tasks/PolynomialProcessorTest.java
@@ -1,13 +1,31 @@
 package ru.omsu.imit.kang.other_tasks;
 
 import org.junit.Test;
+import ru.omsu.imit.kang.quadratic_polynomial.QuadraticPolynomial;
 
 import static org.junit.Assert.*;
 
 public class PolynomialProcessorTest {
 
-    @Test
-    public void getMaxRoot() {
+    @Test(expected = PolynomialProcessorException.class)
+    public void getMaxRootNoRoots() throws PolynomialProcessorException {
+        QuadraticPolynomial p = new QuadraticPolynomial(2,2,1);
+        PolynomialProcessor a = new PolynomialProcessor(p);
+        a.getMaxRoot();
+    }
 
+    @Test
+    public void getMaxRootEqualRoots() throws Exception{
+        QuadraticPolynomial p = new QuadraticPolynomial(1,2,1);
+        PolynomialProcessor a = new PolynomialProcessor(p);
+        assertEquals(-1, a.getMaxRoot(), 1e-9);
+    }
+
+    @Test
+    public void getMaxRoot() throws Exception {
+        QuadraticPolynomial p = new QuadraticPolynomial(1,2,0);
+        PolynomialProcessor a = new PolynomialProcessor(p);
+        assertEquals(0, a.getMaxRoot(), 1e-9);
     }
 }
+
